ce1a6356b879164c9e9cebabe1af8587cf4a167a
w7 <w7@PC230>
2020-12-09 12:56:08 +0700

    qqq

--- WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace WebBanHang.Controllers
             int pageSize = 6;
             int pageNumber = page ?? 1;
             return View(dsSanPhamMoi.ToPagedList(pageNumber,pageSize));
+            //hhh
         }
         
 
